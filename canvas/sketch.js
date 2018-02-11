075219e94f8950f2f265ff7a806b404e70df6b5d
Marcell Sevcsik <sewa972@gmail.com>
2018-02-11 12:05:02 +0100

    Zoom added

--- canvas/sketch.js
+++ canvas/sketch.js
@@ -1,11 +1,12 @@
-var player_unit;
-var enemy_units = [];
-var border;
+let player_unit;
+let enemy_units = [];
+let border;
+let zoom = 1;
 
 function setup() 
 {
 	createCanvas(CANVAS_SIZE_X, CANVAS_SIZE_Y);	// Creates the area that the player sees
-	border = new Border();					// Creates the borders of the map
+	border = new Border();						// Creates the borders of the map
 	player_unit = new Unit(0, 0, 1); 			// Creates the player unit
 	for (let i = 0; i < 10; i++) 
 	{
@@ -20,8 +21,14 @@ function setup()
 function draw() 
 {
 	background(CANVAS_COLOR);
-	translate(width/2 - player_unit.body_position.x, height/2 - player_unit.body_position.y); // Translates the canvas so the player is always in the middle of the screen
+	
+	translate(width/2, height/2); 			// Translates the canvas so the player is always in the middle of the screen (the other translate* command is also needed for this to happen)
+	let update_zoom = (CANVAS_ZOOM_BASE + player_unit.points * CANVAS_ZOOM_MULTIPLIER) / UNIT_RADIUS;
+	zoom = lerp(zoom, update_zoom, 0.2);	// This makes the zoom effect smoother with linear interpolation
+	scale(zoom);  							// Scales (zooms in) according the player points, making the game harder
+	translate(- player_unit.body_position.x, - player_unit.body_position.y); // *
 	border.show();
+	
 	//Iterate over all the enemy units
 	enemy_units.forEach(enemy =>  
 		{
@@ -29,7 +36,7 @@ function draw()
 			//Iterate over all shots of the player unit
 			player_unit.shots.forEach(shot =>
 				{
-					got_hit = enemy.get_hit(shot) // Check if the given shot hit the enemy
+					got_hit = enemy.get_hit(shot) 			// Check if the given shot hit the enemy
 					if(got_hit)
 					{
 						let index = enemy_units.indexOf(enemy);
@@ -37,29 +44,29 @@ function draw()
 						
 						index = player_unit.shots.indexOf(shot);
 						player_unit.shots.splice(index, 1); //Removes the shot from the array (consequently removeing it from the game) 
+						player_unit.points += 1;
 						return;
 					}
 				});
-			if(got_hit){ return; } 	// If the enemy got hit we dont want to draw it, because we removed it from the array earlier, so we have to skip that part or we get an error
-			
-			enemy.show(); // Draws the unit
+			if(got_hit){ return; } 			// If the enemy got hit we dont want to draw it, because we removed it from the array earlier, so we have to skip that part or we get an error
+			enemy.show();  					// Draws the unit
 			if(player_unit.touching(enemy)) // Checks if the player bumped into the enemy 
 			{
 				player_unit.velocity.rotate(PI).setMag(UNIT_BOUNCE_OFF); //Rotates the direction of the players movement making a bounce effect 
 			}
-			if(enemy.shots != undefined)
+			if(enemy.shots != undefined) 
 			{
 				//Iterates over all the shots of the enemy unit
 				enemy.shots.forEach(shot => 
 				{
-					if(shot.ttl == 0) //Checks if the shot has expired ( A shot expires when its ttl = 0)
+					if(shot.ttl == 0) 					//Checks if the shot has expired ( A shot expires when its ttl = 0)
 					{
 						let index = enemy.shots.indexOf(shot);
-						enemy.shots.splice(index, 1); //Removes the shot from the array (consequently removeing it from the game) 
+						enemy.shots.splice(index, 1); 	//Removes the shot from the array (consequently removeing it from the game) 
 					}
 					else
 					{
-						shot.show(); //Draws the shot
+						shot.show(); 					//Draws the shot
 					}
 				});
 			}
@@ -74,7 +81,7 @@ function draw()
 		player_unit.shots.forEach(shot => 
 			{
 				
-				if(shot.ttl == 0) //Checks if the shot has expired ( A shot expires when its ttl = 0)
+				if(shot.ttl == 0) 						//Checks if the shot has expired ( A shot expires when its ttl = 0)
 				{
 					let index = player_unit.shots.indexOf(shot);
 					player_unit.shots.splice(index, 1); //Removes the shot from the array (consequently removeing it from the game) 
